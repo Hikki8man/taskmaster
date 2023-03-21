c4dd065d7817db13fdeba8bb2aa816f480ba513c
ctaleb <taleb.cas@gmail.com>
2023-03-21 15:41:11 +0100

    tiny change to allow autorestart to be lowercase in the yaml file

--- src/task_utils.rs
+++ src/task_utils.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "snake_case")]
 pub enum Autorestart {
 	Always,
 	Unexpected,
